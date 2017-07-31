d059f48ccfbed9b146c2fe89725fbf95d4a3b1ae
mikola1122 <mikola11@i.ua>
2017-08-01 01:55:17 +0300

    Initial kotlin

---
+++ app/src/main/kotlin/com.kardiga.nicolas.kotlindemoapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.kardiga.nicolas.kotlindemoapp
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by nicolas on 01.08.17.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
